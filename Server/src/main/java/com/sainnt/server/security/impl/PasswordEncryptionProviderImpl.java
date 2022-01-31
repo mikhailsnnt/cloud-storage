79da0881ea892b3779cd8bcc364a67a1d68facec
Mihail Nikitin <nitinin.miha@icloud.com>
2022-01-31 18:06:44 +0300

    Added salt

--- Server/src/main/java/com/sainnt/server/security/impl/PasswordEncryptionProviderImpl.java
+++ Server/src/main/java/com/sainnt/server/security/impl/PasswordEncryptionProviderImpl.java
@@ -1,23 +1,29 @@
 package com.sainnt.server.security.impl;
 
 import com.sainnt.server.security.PasswordEncryptionProvider;
-import io.netty.buffer.ByteBuf;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
 
 public class PasswordEncryptionProviderImpl implements PasswordEncryptionProvider {
-    private MessageDigest messageDigest;
-    {
+    private final byte[] salts;
+    private final MessageDigest messageDigest;
+    public PasswordEncryptionProviderImpl() {
         try {
             messageDigest = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
+            salts = new FileInputStream(Objects.requireNonNull(getClass().getResource("/slt")).getFile()).readAllBytes();
+        } catch (NoSuchAlgorithmException | IOException e) {
             e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
     @Override
     public byte[] getEncryptedPassword(byte[] password) {
+        messageDigest.update(salts);
         return  messageDigest.digest(password);
     }
 }
