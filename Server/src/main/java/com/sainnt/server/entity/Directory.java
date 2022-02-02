6fb565145adfe0372e1805f64ef1cd942397a143
Mihail Nikitin <nitinin.miha@icloud.com>
2022-02-02 21:58:48 +0300

    Redefined filesystem storage logic

---
+++ Server/src/main/java/com/sainnt/server/entity/Directory.java
@@ -0,0 +1,60 @@
+package com.sainnt.server.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "directories")
+public class Directory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "dirs_owners",
+            joinColumns = {
+                    @JoinColumn (name = "dir_id")},
+            inverseJoinColumns = {
+                    @JoinColumn(name = "user_id")
+            }
+    )
+    @ToString.Exclude
+    private Set<User> owner;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ToString.Exclude
+    private Directory parent;
+
+    @OneToMany( mappedBy = "parent",fetch = FetchType.LAZY, cascade = {CascadeType.REMOVE})
+    @ToString.Exclude
+    private Set<Directory> subDirs;
+
+    @OneToMany(mappedBy = "parentDirectory", fetch = FetchType.LAZY, cascade = {CascadeType.REMOVE})
+    @ToString.Exclude
+    private Set<File> files;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Directory directory = (Directory) o;
+        return  Objects.equals(id, directory.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
---
+++ Server/src/main/java/com/sainnt/server/entity/File.java
@@ -0,0 +1,48 @@
+package com.sainnt.server.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+
+@Entity
+@Table(name = "files")
+public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private long size;
+    @Column(name = "is_complete")
+    private boolean isComplete;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "owner_id",referencedColumnName = "id")
+    private User owner;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "dir_id",referencedColumnName = "id")
+    private Directory parentDirectory;
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        File file = (File) o;
+        return  Objects.equals(id, file.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+}
