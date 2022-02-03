2607861f0bb3ed0f096d819b45b836eef8b6565d
Mihail Nikitin <nitinin.miha@icloud.com>
2022-02-03 09:01:30 +0300

    Added springframework

--- Server/src/main/java/com/sainnt/server/CloudServer.java
+++ Server/src/main/java/com/sainnt/server/CloudServer.java
@@ -1,32 +1,31 @@
 package com.sainnt.server;
 
-import com.sainnt.server.dao.UserRepository;
-import com.sainnt.server.dao.impl.UserRepositoryImpl;
 import com.sainnt.server.handler.LoginHandler;
+import com.sainnt.server.handler.RootHandler;
 import com.sainnt.server.pipebuilder.PipeLineBuilder;
-import com.sainnt.server.pipebuilder.impl.PipeLineBuilderImpl;
-import com.sainnt.server.security.PasswordEncryptionProvider;
-import com.sainnt.server.security.impl.PasswordEncryptionProviderImpl;
 import com.sainnt.server.service.AuthenticationService;
-import com.sainnt.server.service.impl.AuthenticationServiceImpl;
 import com.sainnt.server.util.HibernateUtil;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+@Component
 public class CloudServer {
-    private final int port;
-    private AuthenticationService authenticationService;
-    private PipeLineBuilder pipeLineBuilder;
-    public CloudServer(int port) {
-        this.port = port;
+    private  int port = 9096;
+
+    private final PipeLineBuilder pipeLineBuilder;
+    private final AuthenticationService authenticationService;
+
+    public CloudServer(AuthenticationService authenticationService, PipeLineBuilder pipeLineBuilder) {
+        this.authenticationService = authenticationService;
+        this.pipeLineBuilder = pipeLineBuilder;
     }
 
     public void run () throws Exception{
         HibernateUtil.getSessionFactory().openSession();
-        injectDependencies();
         EventLoopGroup acceptGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try{
@@ -35,10 +34,13 @@ public class CloudServer {
                     .group(acceptGroup,workerGroup)
                     .channel(NioServerSocketChannel.class)
                     .childHandler(new ChannelInitializer<SocketChannel>() {
+
                         @Override
                         protected void initChannel(SocketChannel socketChannel) {
-                            socketChannel.pipeline().addLast(new LoginHandler(4,pipeLineBuilder,authenticationService));
+                            socketChannel.pipeline().addLast(new RootHandler());
+                            socketChannel.pipeline().addLast(new LoginHandler(pipeLineBuilder, authenticationService));
                         }
+
                     })
                     .option(ChannelOption.SO_BACKLOG,120)
                     .childOption(ChannelOption.SO_KEEPALIVE, true);
@@ -52,20 +54,20 @@ public class CloudServer {
         }
     }
 
-    private void injectDependencies(){
-        UserRepository userRepository = new UserRepositoryImpl();
-        PasswordEncryptionProvider passwordEncryptionProvider = new PasswordEncryptionProviderImpl();
-        authenticationService = new AuthenticationServiceImpl(userRepository, passwordEncryptionProvider);
-        pipeLineBuilder = new PipeLineBuilderImpl(authenticationService);
-
-
+    public void setPort(int port) {
+        this.port = port;
     }
 
     public static void main(String[] args) throws Exception {
         int port = 9096;
         if(args.length>0)
             port = Integer.parseInt(args[0]);
-        (new CloudServer(port)).run();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.scan("com.sainnt.server");
+        context.refresh();
+        CloudServer server = context.getBean(CloudServer.class);
+        server.setPort(port);
+        server.run();
     }
 
 }
