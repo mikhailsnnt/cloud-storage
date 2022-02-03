1ccd21e92e4fb12321d3abc26f8f462272b428f9
Mihail Nikitin <nitinin.miha@icloud.com>
2022-02-03 09:06:13 +0300

    Annotated @Component

--- Server/src/main/java/com/sainnt/server/security/impl/PasswordEncryptionProviderImpl.java
+++ Server/src/main/java/com/sainnt/server/security/impl/PasswordEncryptionProviderImpl.java
@@ -1,13 +1,14 @@
 package com.sainnt.server.security.impl;
 
 import com.sainnt.server.security.PasswordEncryptionProvider;
+import org.springframework.stereotype.Component;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
-
+@Component
 public class PasswordEncryptionProviderImpl implements PasswordEncryptionProvider {
     private final byte[] salts;
     private final MessageDigest messageDigest;
